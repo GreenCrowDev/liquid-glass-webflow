8909d787f43e0783ff8b8099269f496f41ebb2b2
GreenCrowDev <pcalifano@outlook.it>
2026-05-25 18:15:19 +0200

    Three.js minimal shader setup

---
+++ README.md
@@ -0,0 +1,35 @@
+```
+docker pull node:24-slim
+
+docker run -it --rm --entrypoint sh node:24-slim
+```
+
+```
+docker run -it --rm ^
+  -v "D:\repos\work\liquid-glass-webflow:/app" ^
+  -w /app ^
+  --entrypoint sh node:24-slim
+```
+
+Use this, otherwise you won't be able to see the website on your machine port 3000:
+
+```
+docker run -it --rm ^
+  -p 3000:3000 ^
+  -v "D:\repos\work\liquid-glass-webflow:/app" ^
+  -w /app ^
+  node:24-slim sh
+```
+
+Make sure that the package.json has:
+
+```
+"dev": "vite --host 0.0.0.0 --port 3000",
+```
+
+Then in the terminal connected to the docker VM:
+
+```
+npm install
+npm run dev
+```
--- src/main.js
+++ src/main.js
@@ -1,3 +1,54 @@
 import './styles/style.css'
 
-console.log('Hello from VS Code')
+// document.body.innerHTML = "Vite is running"
+console.log('Hello from me')
+
+import * as THREE from 'three';
+
+import vertexShader from './shaders/vertex.glsl?raw'
+import fragmentShader from './shaders/fragment.glsl?raw'
+
+let camera, scene, renderer;
+
+let uniforms;
+
+init();
+
+function init() {
+    const container = document.getElementById('container');
+
+    camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
+    scene = new THREE.Scene();
+
+    const geometry = new THREE.PlaneGeometry(2, 2);
+
+    uniforms = {
+        time: { value: 1.0 }
+    };
+
+    const material = new THREE.ShaderMaterial( {
+        uniforms: uniforms,
+        vertexShader: vertexShader,
+        fragmentShader: fragmentShader
+    } );
+
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setAnimationLoop(animate);
+    container.appendChild(renderer.domElement);
+
+    window.addEventListener('resize', onWindowResize);
+}
+
+function onWindowResize() {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate() {
+    uniforms['time'].value = performance.now() / 1000;
+    renderer.render(scene, camera);
+}
